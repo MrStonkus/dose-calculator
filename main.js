db3dbb814ce0876065e3a7f35714112eeaf03489
Valdas Stonkus <valdas@stonkus.lt>
2021-07-31 15:19:06 +0300

    up

--- main.js
+++ main.js
@@ -84,91 +84,28 @@ fill_doses(new Dose(0, []), 0)
 doses.sort((l, r) => r.mg - l.mg)
 console.log(doses);
 
-// let sizes_array = Array.from(doses)
-// for (let size1 of sizes_array) {
-//   for (let size2 of sizes_array) {
-//     //create temporary dose
-//     let temp_dose = new Dose(size1.mg, [...size1.drugs])
-//     //continue until mg is lower max mg
-//     while (temp_dose.mg + size2.mg <= max_dose_mg) {
-//       //merge tempporary dose with one of existing dose in doses array
-//       temp_dose.mg = temp_dose.mg + size2.mg
-//       temp_dose.drugs = temp_dose.drugs.concat(size2.drugs)
-//       //check the same mg in doses array
-//       if (!doses.find((e) => e.mg === temp_dose.mg)) {
-//         let new_dose = new Dose(temp_dose.mg, [...temp_dose.drugs])
-//         doses.push(new_dose)
-//       }
-//     }
-//   }
-// }
-// doses.sort((a, b) => a.mg - b.mg)
-// console.log(doses);
 
-// function doseOptimizator(dose) {
-  
-//   console.log(dose);
-// }
+// Define daily doses
+var daily_doses = []
+var balance = 0
+for (let i = 0; i < number_of_days_to_calculate_doses; i++) {
+  balance += recomended_daily_dose
 
+  let closest_size = 0
+  let smallest_diff = 9999
+  for (let size of doses) {
+    let diff = Math.abs(size.mg - balance)
+    if (diff < smallest_diff) {
+          smallest_diff = diff
+          closest_size = size.mg
+    }
+  }
+  daily_doses.push(closest_size)
+  balance -= closest_size
+}
+console.log(daily_doses)
 
-// doses.forEach(d => doseOptimizator(d))
-
-// //Expand parts variants
-// let sizes_array = Array.from(doses_array)
-// for (let size1 of sizes_array) {
-//   for (let size2 of sizes_array) {
-//     let new_dose = {
-//       mg: size1.mg,
-//       parts: [size1.drug_part],
-//     }
-//     while (new_dose.mg + size2.mg <= max_dose_mg) {
-//       new_dose.mg = new_dose.mg + size2.mg
-//       new_dose.parts.push(size2.drug_part)
-//       //TODO reikia tobulinti paieska, kad parinktu su maziau objektu
-//       if (!mg_array.find((e) => e === new_dose.mg)) {
-//         mg_array.push(new_dose.mg)
-//         drug_parts.push(new_dose)
-//         new_dose.mg = new_dose.mg
-
-//         let dose = createDose(new_dose)
-//         doses_array.push(dose)
-//       }
-//     }
-//   }
-// }
-// //doses optimizator
-// doses_array.sort((a, b) => a.mg - b.mg)
-// console.log('Dose variants: ')
-// console.log(Array.from(doses_array))
-
-// //TODO mintis cia del paieskos
-// // console.log(doses_array[14].drug_part.parts.length)
-
-
-
-
-// // // Define daily doses
-// // var daily_doses = []
-// // var balance = 0
-// // for (let i = 0; i < number_of_days_to_calculate_doses; i++) {
-// //   balance += recomended_daily_dose
-
-// //   let closest_size = 0
-// //   let smallest_diff = 9999
-// //   for (let size of doses_array) {
-// //     let diff = Math.abs(size - balance)
-// //     if (diff < smallest_diff) {
-// //       smallest_diff = diff
-// //       closest_size = size
-// //     }
-// //   }
-
-// //   daily_doses.push(closest_size)
-// //   balance -= closest_size
-// // }
-// // console.log(daily_doses)
-
-// // //calculate actual conmgption
+//calculate actual consumption
 // // var mg_of_conmgption = 0
 // // for (let i = 0; i < daily_doses.length; i++) {
 // //   mg_of_conmgption = mg_of_conmgption + daily_doses[i]
